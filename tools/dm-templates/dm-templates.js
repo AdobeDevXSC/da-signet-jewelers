f33c935f7aac8c01e95f67f62e8f9910300451b6
Lamont Crook <lamont@adobe.com>
2025-12-12 13:45:42 -0700

    update dm plugin

--- tools/dm-templates/dm-templates.js
+++ tools/dm-templates/dm-templates.js
@@ -1,26 +1,59 @@
 import DA_SDK from 'https://da.live/nx/utils/sdk.js';
+//import { LitElement, html, nothing } from 'da-lit';
+//import { loadPageTags, loadGenTags, savePageTags } from './utils.js';
+
+// Super Lite components
+import 'https://da.live/nx/public/sl/components.js';
+
+// Application styles
+//import loadStyle from '../../scripts/utils/styles.js';
+//const styles = await loadStyle(import.meta.url);
+
+// class DMTemplates extends LitElement {
+//   static properties = {
+//     path: { attribute: false },
+//     token: { attribute: false },
+//     actions: { attribute: false },
+//   };
+//   constructor() {
+//     super();
+//     this.path = '';
+//     this.token = '';
+//     this.actions = {};
+//   }
+//   render() {
+//     return html`
+//       <div id="dm-templates">
+//         <ul class="da-group-list">
+//         </ul>
+//       </div>
+//     `;
+//   }
+// }
+
+//customElements.define('dm-templates', DMTemplates);
 
 (async function init() {
   console.log('init');
-  const { context, token, actions } = await DA_SDK;
-  Object.keys(context).forEach((key) => {
-    // Heading
-    const h3 = document.createElement('h3');
-    h3.textContent = `${key}`;
-
-    // Send button
-    const send = document.createElement('button');
-    send.textContent = `Send | ${context[key]}`;
-    send.addEventListener('click', () => { actions.sendText(context[key]); });
-
-    // Send & close
-    const close = document.createElement('button');
-    close.textContent = `Send & close | ${context[key]}`;
-    close.addEventListener('click', () => {
-      actions.sendText(context[key]);
-      actions.closeLibrary();
+  fetch('/extras/dm-templates.json').then(response => response.json()).then(({ data }) => {
+    console.log(data);
+    data.forEach(({name, dmUrl}) => {
+      const li = document.createElement('li');
+      li.classList.add('da-group-item');
+      const button = document.createElement('button');
+      button.classList.add('da-group-button');
+      const thumbnail = document.createElement('img');
+      thumbnail.src = dmUrl;
+      thumbnail.alt = name;
+      button.appendChild(thumbnail);
+      const title = document.createElement('span');
+      title.classList.add('da-group-title-text');
+      title.textContent = name;
+      button.appendChild(title);
+      li.appendChild(button);
+      document.querySelector('.da-group-list').appendChild(li);
     });
-
-    document.body.append(h3, send, close);
+  }).catch(error => {
+    console.error(error);
   });
 }());
